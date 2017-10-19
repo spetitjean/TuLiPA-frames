bd913606d8596bfe46a8d8d4a91d26ea2d222ddc
davidarps <david-arps@gmx.de>
2017-10-19 15:17:14 +0200

    renamed private printing method in FSPrinter

--- src/de/duesseldorf/frames/FSPrinter.java
+++ src/de/duesseldorf/frames/FSPrinter.java
@@ -1,3 +1,32 @@
+/**
+ * File FSPrinter.java
+ * 
+ * Authors:
+ * David Arps <david.arps@hhu.de>
+ * Simon Petitjean <petitjean@phil.hhu.de>
+ * 
+ * Copyright
+ * David Arps, 2017
+ * Simon Petitjean, 2017
+ * 
+ * This file is part of the TuLiPA-frames system
+ * https://github.com/spetitjean/TuLiPA-frames
+ * 
+ * 
+ * TuLiPA is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TuLiPA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package de.duesseldorf.frames;
 
 import java.util.Iterator;
@@ -51,24 +80,24 @@ public class FSPrinter {
     private static String printFS(Fs fs, int recursiondepth) {
 
         StringBuffer sb = new StringBuffer();
-	recursiondepth++;
+        recursiondepth++;
         if (fs.isTyped()) {
-            sb.append(fs.getCoref()+":");
+            sb.append(fs.getCoref() + ":");
             sb.append("<br>");
-            sb.append(appRecDepth(recursiondepth));
+            sb.append(nonBreakingSpace(recursiondepth));
             sb.append("type: ");
             sb.append(fs.getType().toString());
             sb.append("</br>");
         }
-	
+
         Set<String> keys = fs.getAVlist().keySet();
         Iterator<String> i = keys.iterator();
         while (i.hasNext()) {
             String k = i.next();
             sb.append("<br>");
-            sb.append(appRecDepth(recursiondepth));
+            sb.append(nonBreakingSpace(recursiondepth));
             sb.append(k);
-	    String sep=" = ";
+            String sep = " = ";
             sb.append(sep);
 
             Value v = fs.getAVlist().get(k);
@@ -87,9 +116,15 @@ public class FSPrinter {
         return sb.toString();
     }
 
-    private static String appRecDepth(int recursiondepth) {
+    /**
+     * 
+     * @param spaces
+     *            amount of nbsp's
+     * @return
+     */
+    private static String nonBreakingSpace(int spaces) {
         StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < recursiondepth; i++) {
+        for (int i = 0; i < spaces; i++) {
             sb.append("&nbsp;&nbsp;&nbsp;");
         }
         return sb.toString();
