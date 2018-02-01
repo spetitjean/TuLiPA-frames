2e1794bbe666d7e9146e87ccea3b42b8886fccdc
davidarps <david-arps@gmx.de>
2018-02-01 17:11:26 +0100

    added rrg command line option

--- src/de/duesseldorf/ui/CommandLineProcesses.java
+++ src/de/duesseldorf/ui/CommandLineProcesses.java
@@ -46,6 +46,9 @@ public class CommandLineProcesses {
     public static CommandLineOptions processCommandLine(String[] cmdline) {
         // Command line processing
         CommandLineOptions op = new CommandLineOptions();
+        // we declare the optional rrg option to use the RRG parser
+        op.add(CommandLineOptions.Prefix.DASH, "rrg",
+                CommandLineOptions.Separator.BLANK, false);
         // we declare the optional cyktag option to use the SlimTAG parser
         // (which is the default parser for TAG)
         op.add(CommandLineOptions.Prefix.DASH, "cyktag",
@@ -222,6 +225,8 @@ public class CommandLineProcesses {
 
         res += "where OPTIONS are:\n\t";
         res += "for functionalities:\n\t";
+        res += "-rrg      parser for RRG as of Kallmeyer and Osswald (2017), "
+                + "default parsing algorithm for rrg\n\t";
         res += "-cyktag      use CYK parser for TAG, default parsing algorithm\n\t";
         res += "-tag2rcg     (use TAG to RCG conversion)\n\t";
         res += "-nofiltering cancels polarity filtering\n\t";
