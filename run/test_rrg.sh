ec792031ca0ede1232a7e234a7480a1f1d06cf43
davidarps <david-arps@gmx.de>
2019-04-24 12:15:24 +0200

    updated rrg test script and jar

--- run/test_rrg.sh
+++ run/test_rrg.sh
@@ -1,2 +1,2 @@
-java -jar TuLiPA-frames.jar -g resources/test_rrg/test_sisadj.xml -a CL -v -rrg -s "Mary quickly entered theRoom"
+java -jar TuLiPA-frames.jar -g resources/test_rrg/test_sisadj.xml -v -rrg -s "Mary quickly entered theRoom"
 
