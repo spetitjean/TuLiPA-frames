11a4dac6a7d461819c4ec4971b315a35effd4e96
davidarps <david-arps@gmx.de>
2019-07-15 09:00:21 +0200

    more flexible with order of star and bracks for fs in reading of bracketedtrees

--- src/de/duesseldorf/rrg/io/TreeFromBracketedStringRetriever.java
+++ src/de/duesseldorf/rrg/io/TreeFromBracketedStringRetriever.java
@@ -168,10 +168,15 @@ public class TreeFromBracketedStringRetriever {
         if (nodeStringFromResource.contains("[")) {
             int fsStartingPoint = nodeStringFromResource.indexOf("[");
             String fsString = nodeStringFromResource.substring(fsStartingPoint);
-            nodeFs = new FsFromBracketedStringRetriever(fsString)
-                    .createFsFromString();
             nodeStringFromResource = nodeStringFromResource.substring(0,
                     fsStartingPoint);
+            if (fsString.endsWith("]*")) {
+                fsString = fsString.substring(0, fsString.length() - 1);
+                nodeStringFromResource += "*";
+            }
+            nodeFs = new FsFromBracketedStringRetriever(fsString)
+                    .createFsFromString();
+
         }
         if (nodeStringFromResource.endsWith("*")) {
             nodeType = RRGNodeType.STAR;
