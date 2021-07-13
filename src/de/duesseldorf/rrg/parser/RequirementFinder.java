d0fc19358f4d16ce7df672d44c9250055e6bd5c9
davidarps <david-arps@gmx.de>
2021-07-13 15:15:49 +0200

    rm print

--- src/de/duesseldorf/rrg/parser/RequirementFinder.java
+++ src/de/duesseldorf/rrg/parser/RequirementFinder.java
@@ -441,7 +441,6 @@ public class RequirementFinder {
                 .start(gap.start).end(gap.end).ws(true).build();
         Set<RRGParseItem> candidates = chart.findUnderspecifiedItem(model,
                 false);
-	System.out.println("candidates (findCompleteWrappingFillers): "+candidates);
         Set<RRGParseItem> candidatesWithFittingCats = new HashSet<RRGParseItem>();
         for (RRGParseItem item : candidates) {
             boolean gapHasRightLabel = item.getNode().getCategory()
