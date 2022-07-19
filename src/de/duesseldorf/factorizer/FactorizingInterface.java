38cb9829655e75984f72d18cdf5a50fbeaf3a3da
Julia Block <jublo@hhu.de>
2022-07-19 16:25:23 +0200

    Simple Interface to sort anchored trees into equivalence classes and print them

---
+++ src/de/duesseldorf/factorizer/FactorizingInterface.java
@@ -0,0 +1,101 @@
+package de.duesseldorf.factorizer;
+/*
+ *  File FactorizingInterface.java
+ *
+ *  Authors:
+ *     Wolfgang Maier  <wo.maier@uni-tuebingen.de>
+ *     Yannick Parmentier <parmenti@sfs.uni-tuebingen.de>
+ *     David Arps <david.arps@hhu.de>
+ *     Simon Petitjean <petitjean@phil.hhu.de>
+ *     Julia Block <julia.block@hhu.de>
+ *
+ *  Copyright:
+ *     Wolfgang Maier, 2007
+ *     Yannick Parmentier, 2007
+ *     David Arps, 2017
+ *     Simon Petitjean, 2017
+ *     Julia Block, 2022
+ *
+ * Last modified:
+ *     2022
+ *
+ *  This file is part of the TuLiPA-frames system
+ *     https://github.com/spetitjean/TuLiPA-frames
+ *
+ *  TuLiPA is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TuLiPA is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+import de.duesseldorf.rrg.RRGNode;
+import de.duesseldorf.rrg.RRGTree;
+import de.tuebingen.tree.Node;
+
+import java.util.*;
+
+public class FactorizingInterface {
+
+    public List<EqClass> eqClasses = new ArrayList<>();
+
+    public void factorize(Set<RRGTree> anchoredTrees) {
+        for(RRGTree tree : anchoredTrees) {
+            EqClass finClass = checkDaughters((RRGNode)tree.getRoot());
+        }
+        System.out.println("\n FACTORIZED TREES:" + printClasses());
+    }
+
+    private String printClasses() {
+        String classes = "";
+        int i = 0;
+        for (EqClass eqClass : eqClasses) {
+            classes += "\n Tree "+ i + eqClass.print();
+            i++;
+        }
+        return classes;
+    }
+
+    private EqClass checkDaughters(RRGNode root) {
+        ArrayList<EqClass> daughtersEq = new ArrayList<EqClass>();
+        for (Node child: root.getChildren()) {
+            if(child.getChildren().size() > 0) {
+                daughtersEq.add(checkDaughters((RRGNode) child));
+            }
+            else {daughtersEq.add(checkLeaveClasses((RRGNode)child));}
+        }
+        for (EqClass eqClass: eqClasses) {
+            if(eqClass.belongs(root, daughtersEq)) {
+                return eqClass;
+            }
+        }
+        EqClass newClass = new EqClass(daughtersEq, root.getCategory(), root.getType());
+        eqClasses.add(newClass);
+        return newClass;
+    }
+
+    private EqClass checkLeaveClasses(RRGNode leave) {
+        for (EqClass eqClass: getClassesByNumOfDaughters(0)) {
+            if (eqClass.belongs(leave, new ArrayList<>())) {return eqClass;}
+        } EqClass newClass = new EqClass(new ArrayList<>(), leave.getCategory(), leave.getType());
+        eqClasses.add(newClass);
+        return newClass;
+    }
+
+    private ArrayList<EqClass> getClassesByNumOfDaughters(int numDaughters) {
+        ArrayList<EqClass> possClasses = new ArrayList<EqClass>();
+        for (EqClass eqClass: eqClasses) {
+            if(numDaughters == eqClass.numDaughters){possClasses.add(eqClass);}
+        }
+        return possClasses;
+    }
+}
