13ea3115b465c656f024a2a04521184a0eaaef94
Julia Block <jublo@hhu.de>
2022-07-19 16:18:33 +0200

    - Small import change

--- src/de/duesseldorf/rrg/RRGTree.java
+++ src/de/duesseldorf/rrg/RRGTree.java
@@ -1,13 +1,6 @@
 package de.duesseldorf.rrg;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 import de.duesseldorf.rrg.RRGNode.RRGNodeType;
 import de.duesseldorf.util.GornAddress;
